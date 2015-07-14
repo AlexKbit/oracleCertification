ccecb334b1fe50b2a8e89af137ad30c147a0bb12
AlexKbit <sav4enkoaleksandr@gmail.com>
2015-07-14 23:50:18 +0400

    Add new application with Stradm API

--- src/main/java/com/oracle2/java/application/main/expressions/ApplicationPeopleToJSON.java
+++ src/main/java/com/oracle2/java/application/main/expressions/ApplicationPeopleToJSON.java
@@ -2,25 +2,21 @@ package com.oracle2.java.application.main.expressions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class ApplicationSelectPeople {
+public class ApplicationPeopleToJSON {
 
 	public static void main(String[] args) {
-		int age = 20;
-		
 		List<Person> people = new ArrayList<>();
 		people.add(new Person(20));
 		people.add(new Person(21));
 		people.add(new Person(22));
 		people.add(new Person(23));
 		
-		people.stream().filter(p -> p.getAge() > age).forEach(System.out::println);
+		String json = people.stream().map(Person::toString)
+				.collect(Collectors.joining(", ", "[", "]"));
+		System.out.println(json);
 	}
 }
 
-class Person {
-	private int age;	
-	public Person(int age) {this.age = age;}	
-	public int getAge() {return age;}	
-	public String toString() {return String.valueOf(age);}
-}
---
+++ src/main/java/com/oracle2/java/application/main/expressions/ApplicationRunnable.java
@@ -0,0 +1,21 @@
+package com.oracle2.java.application.main.expressions;
+
+public class ApplicationRunnable {
+	static final String NAME = "Alex";
+	static final String SURNAME = "Jonson";
+	
+	public static void main(String[] arg) {
+		
+		MyRunnable r = ApplicationRunnable::work;
+		r.run(NAME,SURNAME);
+	}
+	
+	public static void work(String name, String surname) {
+		System.out.println("Hello "+name+"."+surname);
+	}
+}
+
+interface MyRunnable {
+	void run(String name, String surname);
+}
+
