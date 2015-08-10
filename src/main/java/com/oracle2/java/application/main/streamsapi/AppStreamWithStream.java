3891c4d7caa969a26aee1594bce1667311efe81c
Aleksandr <sav4enkoaleksandr@gmail.com>
2015-08-10 19:19:22 +0300

    Update stream Api

--- src/main/java/com/oracle2/java/application/main/streamsapi/AppStreamWithStream.java
+++ src/main/java/com/oracle2/java/application/main/streamsapi/AppStreamWithStream.java
@@ -1,8 +1,21 @@
 package com.oracle2.java.application.main.streamsapi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AppStreamWithStream {
 
 	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		Stream<Integer> stream = list.stream().filter(x -> x > 1);
+		list = stream.distinct().collect(Collectors.toList());
+		System.out.println(list);
 		
 	}
 }
