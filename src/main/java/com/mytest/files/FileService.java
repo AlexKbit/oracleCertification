f8cae93736466e2b555a669a492b9f5530b7a987
Aleksandr <sav4enkoaleksandr@gmail.com>
2015-08-18 15:16:12 +0300

    Update nio2

--- src/main/java/com/mytest/files/FileService.java
+++ src/main/java/com/mytest/files/FileService.java
@@ -1,13 +1,18 @@
 package com.mytest.files;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
 
 public class FileService {
 	
 	public static final String INPUT_FILES = "./inputFiles";
 	public static final String OUTPUT_FILES = "./outputFiles";
 	
-	public static final String FILE_WITH_LINES = INPUT_FILES.concat("/fileWithLines.txt");
+	public static final String FILE_WITH_LINES = Paths.get(OUTPUT_FILES, "fileWithLines.txt").toString();
+	public static final String OUTPUT_DIRECTORY_WITH_GENERATE = Paths.get(OUTPUT_FILES, "Generate").toString();
 	
 	public static final boolean createOutputDirectories() {
 		return (new File(OUTPUT_FILES)).mkdirs();
@@ -16,6 +21,21 @@ public class FileService {
 	public static final String pathOutputFor(String name) {
 		createOutputDirectories();
 		return OUTPUT_FILES.concat("/").concat(name);
+	}
+	
+	public static final Path generateDirWithFiles() {
+		File dFile = Paths.get(OUTPUT_DIRECTORY_WITH_GENERATE).toFile();
+		dFile.mkdirs();
+		Random rd = new Random();
+		for (int i=0; i< rd.nextInt(5)+5; i++) {
+			File file = Paths.get(dFile.getAbsolutePath().toString(), String.valueOf(rd.nextInt(10000)+1000)).toFile();
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return Paths.get(OUTPUT_DIRECTORY_WITH_GENERATE);
 	}
 
 }
---
+++ src/main/java/com/oracle2/java/application/main/nio2/stream/AppStreamForNIO2.java
@@ -0,0 +1,11 @@
+package com.oracle2.java.application.main.nio2.stream;
+
+import com.mytest.files.FileService;
+
+public class AppStreamForNIO2 {
+
+	public static void main(String[] args) {
+		FileService.generateDirWithFiles();
+	}
+
+}
