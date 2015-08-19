e5014d84650bb48f69741ad5721f134de6272120
Aleksandr <sav4enkoaleksandr@gmail.com>
2015-08-19 17:39:01 +0300

    Add SQLLite database and application

--- src/main/java/com/oracle2/java/application/main/database/jdbcDriver/AppAddJdbcDriver.java
+++ src/main/java/com/oracle2/java/application/main/database/jdbcDriver/AppAddJdbcDriver.java
@@ -1,11 +1,17 @@
 package com.oracle2.java.application.main.database.jdbcDriver;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AppAddJdbcDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			Class.forName("com.mysql.jdbc.Driver");
+			// OR
+			DriverManager.registerDriver(new org.sqlite.JDBC());
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 			// This Driver-class for database (SQLLite/MySql) and each of this implements java.sql.Driver (JDBC interface)
 			System.out.println("All drivers sucessfuly connected");
 			
