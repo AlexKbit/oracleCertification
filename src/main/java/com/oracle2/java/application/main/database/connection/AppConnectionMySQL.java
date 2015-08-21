7d9d0ab2dfc79482907fe7bfcce2f78077addd4f
Aleksandr <sav4enkoaleksandr@gmail.com>
2015-08-21 19:17:22 +0300

    Update database examples

--- src/main/java/com/oracle2/java/application/main/database/connection/AppConnectionMySQL.java
+++ src/main/java/com/oracle2/java/application/main/database/connection/AppConnectionMySQL.java
@@ -3,8 +3,14 @@ package com.oracle2.java.application.main.database.connection;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import com.mytest.dto.User;
 
 
 public class AppConnectionMySQL {
@@ -22,6 +28,8 @@ public class AppConnectionMySQL {
 		System.out.println("Connection success: "+ !connection.isClosed());
 		showMetaData(connection);
 		
+		getUsers(connection).stream().forEach(System.out::println);
+		
 		connection.close();
 	}
 	
@@ -34,6 +42,19 @@ public class AppConnectionMySQL {
             System.out.println("Product name: " + dm.getDatabaseProductName());
             System.out.println("Product version: " + dm.getDatabaseProductVersion());
         }
+	}
+	
+	private static List<User> getUsers(Connection connection) throws SQLException {
+		String sql = "SELECT * FROM certification_db.users";
+		List<User> users = new ArrayList<>();
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+            users.add(User.create(rs.getInt("id"), rs.getString("name"), rs.getString("surname"), rs.getString("password"), rs.getString("email")));
+        }
+		
+		return users;
 	}
 
 }
